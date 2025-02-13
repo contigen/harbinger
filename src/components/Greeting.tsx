fed3dc61fa1ffae7f3a534ebb65b76656a615ff5
contigen <69436315+contigen@users.noreply.github.com>
2025-02-13 17:28:03 +0100

    Add personalised text

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,11 +1,11 @@
+import { getReturningUserMessage } from '&/lib/utils'
+import { useVisitorData } from '@fingerprintjs/fingerprintjs-pro-react'
 import { motion } from 'framer-motion'
 import { Sparkles } from 'lucide-react'
 
-interface GreetingProps {
-  username?: string
-}
-
-export default function Greeting({ username }: GreetingProps) {
+export default function Greeting() {
+  const { data } = useVisitorData({ extendedResult: true }, { immediate: true })
+  const userId = data?.visitorId
   const greetings = [
     'Welcome to your digital destiny',
     'Prepare for a cosmic tech journey',
@@ -13,7 +13,6 @@ export default function Greeting({ username }: GreetingProps) {
     'Unlock the mysteries of your gadgets',
     'Dive into the digital unknown',
   ]
-
   const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)]
 
   return (
@@ -25,9 +24,17 @@ export default function Greeting({ username }: GreetingProps) {
     >
       <h1 className='flex justify-center items-center mb-2 text-4xl font-bold text-blue-400'>
         <Sparkles className='mr-2' />
-        {username ? `Hello, ${username}!` : 'Greetings, Digital Explorer!'}
+        {userId ? `Hello, User #${userId}!` : 'Greetings, Digital Explorer!'}
       </h1>
-      <p className='text-xl text-gray-300'>{randomGreeting}</p>
+      <p className='text-xl text-gray-300'>
+        {randomGreeting} <br />
+      </p>
+      <p className='text-sm text-gray-200'>
+        {getReturningUserMessage(
+          data?.firstSeenAt?.global ?? undefined,
+          data?.lastSeenAt?.global ?? undefined
+        )}
+      </p>
     </motion.div>
   )
 }
